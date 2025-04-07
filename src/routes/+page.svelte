28a28b524a31875f5aa93ea35091dfec365d29e8
Anesu Mandaza <anesu203@gmail.com>
2025-04-07 15:36:43 +0200

    scaffold

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,42 @@
   import Carousel from "$lib/components/Carousel.svelte";
   import Chart from "$lib/components/Chart.svelte";
   import Trending from "$lib/components/Trending.svelte";
+  import IconMenu from "$lib/icons/IconMenu.svelte";
 
   let { data } = $props();
-  console.log(data);
+
+  let menuOpen = $state();
 </script>
 
-<Carousel trending={data.trending}></Carousel>
+<!-- <Carousel trending={data.trending}></Carousel>
 <div class="w-full overflow-auto flex space-x-4">
   {#each data.artists as artist}
     <Artist name={artist.name} image={artist.image}></Artist>
   {/each}
-</div>
+</div> -->
 <!-- {#each data.trending as song}
   <Trending title={song.title} image={song.image} artist={song.artist}
   ></Trending>
 {/each} -->
 
-{#each data.charts as chart, i}
+<!-- {#each data.charts as chart, i}
   <Chart
     title={chart.songTitle}
     image={chart.albumArt}
     position={i + 1}
     artist={chart.artist}
   ></Chart>
-{/each}
+{/each} -->
+
+<div class=" bg-purple-500 flex flex-row w-full h-screen">
+  <div class="flex flex-col bg-red-400 w-8 md:w-2/12">
+    <button class="btn btn-ghost btn-square btn-sm"
+      ><IconMenu></IconMenu></button
+    >
+  </div>
+  <div class="bg-green-800 p-4 w-full md:w-10/12">
+    <div class="w-full bg-sky-300 p-4 h-[10%]"></div>
+    <div class="w-full bg-sky-500 p-4 h-[40%]"></div>
+    <div class="w-full bg-sky-700 p-4 h-[50%]"></div>
+  </div>
+</div>
